2be55153f8cdd76a4ea7292c601ee5e40c11af46
Ryan Pavlik <ryan@sensics.com>
2015-04-16 16:17:10 -0500

    Fix configure script

--- configure.cmd
+++ configure.cmd
@@ -2,5 +2,5 @@
 @mkdir "%~dp0build"
 pushd "%~dp0build"
 @rem "-DCMAKE_MAKE_PROGRAM=%ANDROID_NDK%prebuilt\windows-x86_64\bin\make.exe"
-cmake .. -G "MinGW Makefiles" "-DCMAKE_MAKE_PROGRAM=%ANDROID_NDK%prebuilt\windows-x86_64\bin\make.exe" -DCMAKE_BUILD_TYPE=Release
+cmake .. -G "MinGW Makefiles" "-DCMAKE_MAKE_PROGRAM=%ANDROID_NDK%\prebuilt\windows-x86_64\bin\make.exe" -DCMAKE_BUILD_TYPE=Release
 popd
