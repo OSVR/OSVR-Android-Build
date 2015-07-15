7407c1015389e2af8cac0c27dc7b417a6cd505df
Ryan Pavlik <ryan@sensics.com>
2015-07-15 04:10:29 -0500

    Reduce Busybox requirement for install scripts.

--- src/packaging/setup.sh
+++ src/packaging/setup.sh
@@ -2,8 +2,21 @@
 root=$(cd $(dirname $0) && pwd)
 launcher=${root}/bin/launcher.sh
 
+if which chmod > /dev/null; then
+    $CHMOD=chmod
+else
+    $CHMOD=toolbox chmod
+fi
+
+
+if which ln > /dev/null; then
+    $LN=ln
+else
+    $LN=toolbox ln
+fi
+
 make_executable() {
-    busybox chmod +x $@
+    $CHMOD 775 $@
 }
 
 make_executable ${launcher}
@@ -12,7 +25,7 @@ for app in osvr_server osvr_print_tree AnalogCallback_cpp ButtonCallback_cpp Dis
     if [ -e ${root}/bin/${app} ]; then
         make_executable ${root}/bin/${app}
         rm -f ${root}/${app}
-        ln -s ${launcher} ${root}/${app}
+        $LN -s ${launcher} ${root}/${app}
         make_executable ${root}/${app}
     else
         rm -f ${root}/${app}
